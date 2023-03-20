590b23e7c6ca2a76929a9ea80de0e9c592b15834
Rajni <io3.rajni@gmail.com>
2023-03-21 00:39:37 +0530

    javascript assignment

---
+++ assignment/2023-03-19 assignment javascriipt/average.js
@@ -0,0 +1,44 @@
+/** 
+ * @description Function to get student data
+   @param {Number} number0
+ * @param {Number} number1
+ * @param {Number} number2
+ * @param {Number} number3
+ * @param {Number} number4
+ * @param {Number} number5
+ * @param {Number} number6
+ * @param {Number} number7
+ * @param {Number} number8
+ * @param {Number} number9
+ */
+const first = Number(prompt(`Enter First Number`));
+const second = Number(prompt(`Enter second Number`));
+const third = Number(prompt(`Enter third Number`));
+const fourth = Number(prompt(`Enter Fourth Number`));
+const fifth = Number(prompt(`Enter Fifth Number`));
+const sixth = Number(prompt(`Enter sixth Number`));
+const seventh = Number(prompt(`Enter seventh Number`));
+const eighth = Number(prompt(`Enter eighth Number`));
+const ninth = Number(prompt(`Enter ninth Number`));
+const tenth = Number(prompt(`Enter tenth Number`));
+function findAverage(
+  num1,
+  num2,
+  num3,
+  num4,
+  num5,
+  num6,
+  num7,
+  num8,
+  num9,
+  num10
+) {
+  const total = Number(
+    num1 + num2 + num3 + num4 + num6 + num7 + num8 + num9 + num10
+  );
+  console.log(`Sum Of all this Number---> ${total}`);
+  const average = total / 10;
+  console.log(`Average Of all 10 Number -->${average}`);
+}
+
+findAverage(first, second, third, fourth, sixth, seventh, eighth, ninth, tenth);
