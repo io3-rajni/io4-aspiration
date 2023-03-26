538e82a389bfa9a374467d95b2f5c2cc9bef3c58
Rajni <io3.rajni@gmail.com>
2023-03-27 00:22:28 +0530

    assignment submitting

--- assignment/2023-03-25 assignment javascript-4/script.js
+++ assignment/2023-03-25 assignment javascript-4/script.js
@@ -29,6 +29,58 @@ function calculate(s1, s2, s3, s4, s5) {
 }
 // calculate(first, Second, Third, Fourth, fifth);
 
+/**
+ * 5. In a company if an employee is paid as under:
+ * If his basic salary is less than Rs. 1500,
+ * then HRA=10% of basic salary and DA=90% of basic salary.
+ * If his salary is either equal to or above Rs. 1500,
+ * then HRA = Rs. 500 and DA = 98% of basic salary.
+ * If the employee's salary is input through the keyboard
+ *  write a program to find his gross salary.
+ */
+
+const salary = Number(prompt(`Enter basic Salary`));
+function findGrossSalary(bs) {
+  if (bs < 1500) {
+    console.log(`Basic salary is----> ${bs}`);
+    const HRA = (10 / 100) * bs;
+    console.log(`HRA Salary is ${HRA}`);
+    const DA = (90 / 100) * bs;
+    console.log(`DA Salary is---->${DA}`);
+    gs = HRA + DA + bs;
+    console.log(`Gross salary is----->${gs} `);
+  } else if (bs >= 1500) {
+    console.log(`Basic salary is----> ${bs}`);
+    const HRA = 500;
+    console.log(`HRA Salary is ${HRA}`);
+    const DA = (98 / 100) * bs;
+    console.log(`DA Salary is---->${DA}`);
+    gs = HRA + DA + bs;
+    console.log(`Gross salary is--->${gs} `);
+  }
+}
+findGrossSalary(salary);
+/**
+ * 6.if cost price and selling price of an item are input through the keyboard,
+ * write a program to determine whether the seller has made profit of incurred loss.
+ * Also determine how much profit he made or loss he incurred
+ */
+
+// const sellingPrice = Number(prompt(`Enter selling price`));
+// const costPrice = Number(prompt(`Enter cost price`));
+function profitLoss(sp, cp) {
+  if (sp > cp) {
+    const profit = sp - cp;
+    console.log(`Profit is---->${profit}`);
+  } else if (sp < cp) {
+    const loss = cp - sp;
+    console.log(`loss is---->${loss}`);
+  } else if (sp == cp) {
+    const same = sp - cp;
+    console.log(`no Profit no Loss---->${same}`);
+  }
+}
+// profitLoss(sellingPrice, costPrice);
 // 8.Any integer is input through the keyboard. Write a program to find out whether it is an odd number or even number.
 
 // const num = Number(prompt(`Enter any Number and find odd even`));
@@ -58,12 +110,13 @@ function findYear(year) {
 // findYear(leap);
 
 // 11. If the ages of Ram, Shyam and Ajay are input through the keyboard, write a program to determine the youngest of the three.
-const first = Number(prompt(`enter Ram age`));
-console.log(`Ram Age ${first}`);
-const second = Number(prompt(`enter Shaym age`));
-console.log(`Shyam Age ${second}`);
-const third = Number(prompt(`enter ajay age`));
-console.log(`Ajay Age ${third}`);
+
+// const first = Number(prompt(`enter Ram age`));
+// console.log(`Ram Age ${first}`);
+// const second = Number(prompt(`enter Shaym age`));
+// console.log(`Shyam Age ${second}`);
+// const third = Number(prompt(`enter ajay age`));
+// console.log(`Ajay Age ${third}`);
 function ageFind(ram = "", shyam = "", ajay = "") {
   if (ram <= shyam) {
     console.log(`Ram is youngest Shaym`);
@@ -75,4 +128,19 @@ function ageFind(ram = "", shyam = "", ajay = "") {
     return 0;
   }
 }
-ageFind(first, second, third);
+// ageFind(first, second, third);
+
+/**
+ * 12. Write a program to find the absolute value of a number entered through the keyboard
+ */
+const val = Number(prompt(`enter any first number`));
+console.log(`First number is ${val}`);
+const val1 = Number(prompt(`enter  any second number`));
+console.log(`Second number is ${val1}`);
+
+function absolute(num1, num2) {
+  const find = Math.abs(num1 * num2);
+
+  console.log(` Absolute value is---> ${find}`);
+}
+absolute(val, val1);
