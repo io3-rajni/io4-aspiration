b5700a325d209cfa530be873c3762e2622f712a0
Rajni <io3.rajni@gmail.com>
2023-03-29 00:21:36 +0530

    assignment-submitting

--- assignment/2023-03-25 assignment javascript-4/script.js
+++ assignment/2023-03-25 assignment javascript-4/script.js
@@ -32,26 +32,36 @@ function calculate(s1, s2, s3, s4, s5) {
 // 3.  if a five-digit number is input through the keyboard,
 // program to reverse the number.
 
-const num = prompt(`Enter any number`);
-console.log(`Enter Number ${num}`);
+// const num = prompt(`Enter any number`);
+// console.log(`Enter Number ${num}`);
 function rev(num) {
   return String(num).split("").reverse().join("");
 }
-console.log(`Reversed integer ${Number(rev(num))}`);
+// console.log(`Reversed integer ${Number(rev(num))}`);
 
-// ‘If lengths of three sides of a triangle are input through the
+//4. ‘If lengths of three sides of a triangle are input through the
 // keyboard, write a program to find the area of the triangle.
 
-const first = Number(prompt(`Enter first side `));
-const second = Number(prompt(`Enter second side `));
-const third = Number(prompt(`Enter third side `));
+// const first = Number(prompt(`Enter first side `));
+// const second = Number(prompt(`Enter second side `));
+// const third = Number(prompt(`Enter third side `));
 function areaOfTriangle(s1, s2, s3) {
   let area = (s1 + s2 + s3) / 2;
   console.log(`three side of triangle ${area}`);
   const findArea = Math.sqrt(area * (area - s1) * (area - s2) * (area - s3));
   console.log(`Area of triangle----> ${findArea}`);
 }
-areaOfTriangle(first, second, third);
+// areaOfTriangle(first, second, third);
+
+// 5.  Write a program to receive Cartesian co-ordinates (x, y) of a point
+// and convert them into polar co-ordinates (r, φ- 9)
+// **Hint: r=Math.sqrt(x^2+y^2)and φ =Math.tan^-1 (y/X)**
+// const firstVal = Number(prompt(`enter first value`));
+// const secondVal = Number(prompt(`enter second value`));
+function coOrdinates(val1, val2) {
+  const r = Math.sqrt(val1 + val1 + val2 * val2);
+  console.log(`add two value--->${r}`);
+}
 
 /**
  * 5. In a company if an employee is paid as under:
@@ -162,7 +172,7 @@ function ageFind(ram = "", shyam = "", ajay = "") {
 // const val = Number(prompt(`enter any first number`));
 // console.log(`First number is ${val}`);
 // const val1 = Number(prompt(`enter  any second number`));
-// console.log(`Second number is ${val1}`);
+// console.log(`Second number is ${val1}`);2
 
 function absolute(num1, num2) {
   const find = Math.abs(num1 * num2);
@@ -170,3 +180,78 @@ function absolute(num1, num2) {
   console.log(` Absolute value is---> ${find}`);
 }
 // absolute(val, val1);
+
+// 16. A company insures its drivers in the following cases: -
+// _If the driver is married._
+// _If the driver is unmarried , male & above 30 years of age._
+// _If the driver is unmarried, female & above 25 years of age._
+// In all other cases, the driver is not insured. If the marital status, sex and age of the driver are the inputs, write a program to determine whether the driver should be insured or not.
+
+// const gender = prompt(`Driver Gender Male/Female `);
+// const mStatus = prompt(`Enter marital Status`);
+// const age = prompt(`Enter Driver age`);
+function insuredOrNot(g, ms, a) {
+  if (g == "male" && ms == "married" && a >= "30") {
+    console.log(
+      ` The Driver is ${g} he is ${ms} and his age is ${a} should not be insured`
+    );
+  } else if (g == "male" && ms == "unmarried" && a <= 30) {
+    console.log(
+      ` The Driver is ${g} he is ${ms} and his age is ${a} should be insured`
+    );
+  } else if (g == "female" && ms == "married" && a >= 30) {
+    console.log(
+      ` The Driver is ${g} her marital Status is ${ms} and his age is ${a} should not be insured`
+    );
+  } else if (g == "female" && ms == "unmarried" && a <= 30) {
+    console.log(
+      ` The Driver is ${g} her marital Status is ${ms} and his age is ${a} should be insured`
+    );
+  }
+}
+// insuredOrNot(gender, mStatus, age);
+
+// 15. The marks obtained by a student in 5 different subjects are input through the keyboard. The student gets a division as per the following rules:
+
+// - Percentage above or equal to 60 - _First division_
+// - Percentage between 50 and 59 - _Second division_
+// - Percentage between 40 and 49 - _Third division_
+// - Percentage less than 40 - _Fail_
+
+/**
+ * @description Function to get student data
+ * @param {number} englishMarks
+
+ * @param {number} mathMarks
+ * @param {number} physicsMarks
+ * @param {number} chemistryMarks
+ * @param {number} computerMarks
+ *
+ *
+ */
+const english = Number(prompt(`enter english marks`));
+const maths = Number(prompt(`enter Math marks`));
+const physics = Number(prompt(`enter physics marks`));
+const chemistry = Number(prompt(`enter chemistry marks`));
+const computer = Number(prompt(`enter Computer marks`));
+
+function findDivision(eng = "", math = "", phy = "", chem = "", com = "") {
+  let obtMarks = eng + math + phy + chem + com;
+  console.log(`Obtained  Marks is ---->${obtMarks}`);
+  let total = 500;
+  console.log(`Total Marks is ---->${total}`);
+  let per = (obtMarks / total) * 100;
+  console.log(`Percentage--->${per}`);
+  if (per >= 60) {
+    console.log(`First Division`);
+  } else if (per >= 50 && per <= 59) {
+    console.log(`Second Division`);
+  } else if (per >= 40 && per <= 49) {
+    console.log(`Third Division`);
+  } else if (per <= 40) {
+    console.log(`Fail`);
+  } else {
+    console.log(`Try next time`);
+  }
+}
+findDivision(english, maths, physics, chemistry, computer);
---
+++ assignment/2023-03-28/script.js
@@ -0,0 +1,10 @@
+// alert("-----");
+for (var i = 1; i <= 10; i++) {
+  console.log(`2 x ${i} = ${2 * i}`);
+}
+// for (let i = 10; i >= 1; i--) {
+//   console.log(`3 x ${i} = ${2 * 1}`);
+}
+for (var i = 1; i <= 50; i++) {
+  console.log(`21 x ${i}=${21 * i}`);
+}
