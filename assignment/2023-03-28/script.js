269dea8d82ee184bebbc9e26c7cbaac1892383ba
Rajni <io3.rajni@gmail.com>
2023-03-29 09:16:16 +0530

    class-assignment

--- assignment/2023-03-28/script.js
+++ assignment/2023-03-28/script.js
@@ -1,10 +1,25 @@
 // alert("-----");
-for (var i = 1; i <= 10; i++) {
-  console.log(`2 x ${i} = ${2 * i}`);
-}
-// for (let i = 10; i >= 1; i--) {
-//   console.log(`3 x ${i} = ${2 * 1}`);
-}
-for (var i = 1; i <= 50; i++) {
-  console.log(`21 x ${i}=${21 * i}`);
+// for (var i = 1; i <= 10; i++) {
+//   console.log(`2 x ${i} = ${2 * i}`);
+// }
+// // for (let i = 10; i >= 1; i--) {
+// //   console.log(`3 x ${i} = ${2 * 1}`);
+// }
+// for (var i = 1; i <= 50; i++) {
+//   console.log(`21 x ${i}=${21 * i}`);
+// }
+
+// for (let i = 2000; i <= 3000; i++) {
+// if (i % 4 === 0) console.log(`leap year ${i}`);
+// }
+//next program
+const year = Number(prompt(`year start`));
+const year1 = Number(prompt(`year  end`));
+for (let i = year; i <= year1; i++) {
+  if (i % 4 == 0) {
+    console.log(`Find leap Year${i}`);
+  }
+  for (let i =0; i <= .length(); i++) {
+    console.log(`Find lengthYear${i}`);
+  }
 }
