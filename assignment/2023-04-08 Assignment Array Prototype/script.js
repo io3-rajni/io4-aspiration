14be810a34806222a325181acf7370917b1456e0
Rajni <io3.rajni@gmail.com>
2023-04-12 00:51:12 +0530

    assignment-submitting

--- assignment/2023-04-08 Assignment Array Prototype/script.js
+++ assignment/2023-04-08 Assignment Array Prototype/script.js
@@ -145,8 +145,6 @@ console.log("Fifth Find Index-->", fifth5.indexOf("Sweaters"));
 
 // 9. Find the last index in the following arrays.
 const lastIdx = [210, "Sunday", 702, true, null, "90", "vinay", "CAT"]; //find last index of (‘vinay’, null,‘Ajay’
-console.log(`find last Index-->`, lastIdx.lastIndexOf(210));
-console.log(`find last Index-->`, lastIdx.lastIndexOf(null));
 
 const lastIdx1 = [
   "Sunday",
@@ -160,3 +158,84 @@ const lastIdx1 = [
 const lastIdx2 = ["z", "i", "a", "x", "k", "s"]; //find last index of (‘k’, ‘t’, ‘z’)
 const lastIdx3 = [115, 54, 101, -52, -10, 0.56, 5, 12, 8, 130, 44]; //find last index of (115,54,0.56, 81)
 const lastIdx4 = ["shoes", "shirts", "socks", "sweaters"]; //find index of (‘shoes,socks’,‘Sweaters’)
+console.log(
+  `find last Index-->`,
+  lastIdx.lastIndexOf("vinay"),
+  lastIdx1.lastIndexOf("Tuesday"),
+  lastIdx2.lastIndexOf("k"),
+  lastIdx3.lastIndexOf(115),
+  lastIdx4.lastIndexOf("shoes")
+);
+console.log(
+  `find last Index-->`,
+  lastIdx.lastIndexOf(null),
+  lastIdx1.lastIndexOf("TuesDay"),
+  lastIdx2.lastIndexOf("t"),
+  `--->3`,
+  lastIdx3.lastIndexOf(54),
+  `----->4`,
+  lastIdx4.lastIndexOf("sweaters")
+);
+console.log(
+  `find last Index-->`,
+  lastIdx.lastIndexOf("Ajay"),
+  lastIdx1.lastIndexOf("sunday"),
+  lastIdx2.lastIndexOf(0.56)
+);
+
+console.log(
+  `find last Index-->`,
+  lastIdx1.lastIndexOf("Wednesday"),
+  lastIdx3.lastIndexOf(81)
+);
+
+//10. Find the index in the arrays.
+// If element > 11
+// b. If element * 10 +5 >= 100
+// c. If element is less than 0
+// d. If element is equal to 8
+// e. If element is equal to 11 OR 12
+const findInd = [314, 115, 54, 101, -52, -10, 0.56, 5, 12, 8, 130, 44];
+const newVar = [];
+// console.log(findInd.push(newVar));
+const findIndex = (array) => {
+  array.map((e, i) => {
+    switch (e) {
+      case e > 11:
+        newVar = e;
+        //  newVar;
+        return newVar.push(e);
+        break;
+      default:
+        return 0;
+    }
+  });
+};
+console.log(newVar);
+console.log(findIndex(findInd));
+
+// 11. Please verify if following includes in array
+const includes = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+console.log(includes.includes("Funday", "Sunday", "Wednesday"));
+// (‘Sunday’, ‘wednesday’, ‘Funday’)
+const includes1 = [101, -52, -10, 0.56, 5]; // if includes → (5, 8, ‘10’, 100+1, 10-5)
+
+const isIncludes = (array, checkInclude) => {
+  console.log(`${checkInclude} is include`, array.includes(checkInclude));
+};
+isIncludes(includes, "Sunday");
+isIncludes(includes, "Funday");
+isIncludes(includes, "Wednesday");
+isIncludes(includes1, 5);
+isIncludes(includes1, 8);
+isIncludes(includes1, "10");
+isIncludes(includes1, 100 + 1);
+isIncludes(includes1, 10 - 5);
